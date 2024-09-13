c8ffc9171f2c1c689e6ff36c67941c99d1d9f2de
athaydaooo <athaydaooo@gmail.com>
2024-09-13 14:43:15 -0300

    docs/added swagger route and fixed dev error

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -4,7 +4,9 @@ import cors from 'cors'
 
 import { errorHandler } from './middleware/error-handler'
 import { routes } from './routes'
-import { bearerVerifier } from './middleware/bearer-verifier'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../../docs/swagger-output.json'
+
 
 const internalPort = process.env.API_INTERNAL_PORT || 3000
 const externalPort = process.env.API_EXTERNAL_PORT || 3000
@@ -15,12 +17,15 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(routes)
 
 app.use(errorHandler)
 
-app.use(bearerVerifier)
-
 app.listen(process.env.API_INTERNAL_PORT || 3000, () => {
   console.log(`🚀 Server started on container port ${internalPort} and external port ${externalPort}`)
 })
+
+//added because restarts when file saves were not working during yarn run dev
+process.on('SIGTERM', () => process.kill(process.pid, 'SIGINT'))
